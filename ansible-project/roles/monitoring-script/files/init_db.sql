a78a20cb41c580a9a1ebd0129995b31889bb9cc7
Theophilus Ijiebor <theophilus@Theophiluss-MacBook-Pro.local>
2025-09-22 22:38:37 -0400

    Scaffold Ansible project, roles, playbooks; add monitoring files/templates

---
+++ ansible-project/roles/monitoring-script/files/init_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS syslogs;
+USE syslogs;
+
+CREATE TABLE IF NOT EXISTS stats (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  timestamp DATETIME,
+  cpu_usage FLOAT,
+  memory_usage FLOAT,
+  host VARCHAR(128)
+);
